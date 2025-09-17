ca383633eb1abdeed86fa92ce5aa1a3364853446
Nguyen Ngoc <ngoc.n@shopstack.asia>
2025-09-18 00:01:17 +0700

    Add authen sha1, and ui template

--- SD_Lite/SD_Lite.ino
+++ SD_Lite/SD_Lite.ino
@@ -5,60 +5,122 @@
 #include "FS.h"
 #include "SD.h"
 #include "SPI.h"
-
+#include <SHA1Builder.h>
 
 int sck = 18;
 int miso = 19;
 int mosi = 23;
 int cs = 5;
 
+String secret_token_hex = "5715790a892990382d98858c4aa38d0617151575";
+
 const char *ssid = "Engine194";
 const char *password = "Engine194";
+
+static unsigned char _bearer[20];
 
 WebServer server(80);
 
 const char *www_username = "admin";
-const char *www_password = "esp32";
+const char *www_password = "6adfb183a4a2c94a2f92dab5ade762a47889a5a1";
 
-void readFile(fs::FS &fs, const char *path) {
+String *check_bearer_or_auth(HTTPAuthMethod mode, String authReq, String params[]) {
+  // we expect authReq to be "bearer some-secret"
+  String lcAuthReq = authReq;
+  lcAuthReq.toLowerCase();
+  if (mode == OTHER_AUTH && (lcAuthReq.startsWith("bearer "))) {
+    String secret = authReq.substring(7);
+    secret.trim();
+
+    uint8_t sha1[20];
+    SHA1Builder sha_builder;
+
+    sha_builder.begin();
+    sha_builder.add((uint8_t *)secret.c_str(), secret.length());
+    sha_builder.calculate();
+    sha_builder.getBytes(sha1);
+
+    if (memcmp(_bearer, sha1, sizeof(_bearer)) == 0) {
+      Serial.println("Bearer token matches");
+      return new String("anything non null");
+    } else {
+      Serial.println("Bearer token does not match");
+    }
+  } else if (mode == BASIC_AUTH) {
+    bool ret = server.authenticateBasicSHA1(www_username, www_password);
+    if (ret) {
+      Serial.println("Basic auth succeeded");
+      return new String(params[0]);
+    } else {
+      Serial.println("Basic auth failed");
+    }
+  }
+
+  // No auth found
+  return NULL;
+};
+
+void readFile(fs::FS &fs, const char *path)
+{
   Serial.printf("Reading file: %s\n", path);
 
   File file = fs.open(path);
-  if (!file) {
+  if (!file)
+  {
     Serial.println("Failed to open file for reading");
     return;
   }
 
   Serial.print("Read from file: ");
-  while (file.available()) {
+  while (file.available())
+  {
     Serial.write(file.read());
   }
   file.close();
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
 
   SPI.begin(sck, miso, mosi, cs);
-  if (!SD.begin()) {
+  if (!SD.begin())
+  {
     Serial.println("Card Mount Failed");
     return;
   }
-  if (!WiFi.softAP(ssid, password)) {
+  if (!WiFi.softAP(ssid, password))
+  {
     log_e("Soft AP creation failed.");
     while (1);
   }
+  
   IPAddress myIP = WiFi.softAPIP();
   ArduinoOTA.begin();
 
+  // Convert token to a convenient binary representation.
+  size_t len = HEXBuilder::hex2bytes(_bearer, sizeof(_bearer), secret_token_hex);
+  if (len != 20) {
+    Serial.println("Bearer token does not look like a valid SHA1 hex string ?!");
+  }
 
-  server.on("/", []() {
-    readFile(SD, "/admin.env.json");
-    if (!server.authenticate(www_username, www_password)) {
-      return server.requestAuthentication();
-    }
-    server.send(200, "text/plain", "Login OK");
-  });
+  server.on("/", HTTP_GET, []()
+    {
+      if (!server.authenticate(&check_bearer_or_auth)) {
+        Serial.println("No/failed authentication");
+        return server.requestAuthentication();
+      }
+      Serial.println("Authentication succeeded");
+      File file = SD.open("/ui/index.html", "r");
+      if (file) {
+          server.streamFile(file, "text/html");
+          file.close();
+      } else {
+          server.send(404, "text/plain", "Page Not Found");
+      }
+    });
+
+  server.serveStatic("/", SD, "/ui/");
   server.begin();
 
   Serial.print("Open http://");
@@ -66,8 +128,9 @@ void setup() {
   Serial.println("/ in your browser to see it working");
 }
 
-void loop() {
+void loop()
+{
   ArduinoOTA.handle();
   server.handleClient();
-  delay(2);  //allow the cpu to switch to other tasks
+  delay(2); // allow the cpu to switch to other tasks
 }
